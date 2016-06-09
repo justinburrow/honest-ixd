091ee0d91b3246dc2f556fd97144d712906dbbdf
Justin Burrow <jburrow@honest.com>
2016-06-08 18:14:40 -0700

    Mobile Modal pattern files added

---
+++ app/scripts/modules/mobile-modal.js
@@ -0,0 +1,15 @@
+/* globals angular:false */
+'use strict';
+var app = angular.module('mobileModal', []);
+
+app.controller('mobileModalCtrl', function() {
+  var vm = this;
+  
+  vm.modalOpen = function() {
+    angular.element('.overlay').addClass('overlay--shown');
+  }
+  
+  vm.modalClose = function() {
+    angular.element('.overlay').removeClass('overlay--shown');
+  }
+});
